902cc0f0188afba6dd201b3e1e8e6468b35d0557
yeto-yeto <etochan5@gmail.com>
2021-01-28 22:42:20 +0900

    :tada: setup template

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+const Home = () => {
+  return <h1>Home</h1>
+}
+
+// eslint-disable-next-line import/no-default-export
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media", // 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+};
